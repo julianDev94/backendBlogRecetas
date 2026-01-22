8780ea1342aa233d4adde25230e1c54c1b78ca88
JulianAballay <juliancabj1210@gmail.com>
2026-01-21 21:32:36 -0300

    modifico lo que seria el shchema de la receta ya que las validaciones para los ingredientes y pasos estaban mal definidas

--- src/routes/receta.routes.js
+++ src/routes/receta.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
+import { crearReceta, editarReceta, eliminarUnaReceta, listaRecetas, obtenerUnaReceta } from "../controllers/receta.controllers.js";
 
 const router = Router();
 
-// router.route('/recetas').get().post();
-// router.route('/receta/:id').get().put().delete();
+router.route('/recetas').get(listaRecetas).post(crearReceta);
+router.route('/receta/:id').get(obtenerUnaReceta).put(editarReceta).delete(eliminarUnaReceta);
 
 export default router;
