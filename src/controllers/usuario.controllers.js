22100f6a26b1834cafbfd2c7b5e17a94838f66da
JulianAballay <juliancabj1210@gmail.com>
2026-01-24 22:35:02 -0300

    completo el metodo login con logica que va desde chequear si el existe en la db, comprobando con el usuario que se obtiene del body y en caso que si, se verifica que el password obtenido del body se la compare con la password de la db que se encuentra hasheada en la base de datos. En caso que existe en responde con un 200 y que el usuario si existe

--- src/controllers/usuario.controllers.js
+++ src/controllers/usuario.controllers.js
@@ -1,15 +1,22 @@
 import Usuario from "../database/model/usuario.js"
+import bcrypt from 'bcrypt';
 
 export const login = async(req,res)=>{
     try {
         const {usuario, pass} = req.body
         const usuarioEncontrado = await Usuario.findOne({usuario});
         if(!usuarioEncontrado){
-            res.status(404).json({mensaje: 'El usuario no se encuentra registado'});
+            res.status(404).json({mensaje: 'Usuario o contraseña invalidos'});
         }
 
+        const passValidado = bcrypt.compareSync(pass, usuarioEncontrado.password);
+        if(!passValidado){
+            res.status(400).json({mensaje: 'Usuario o contraseña invalidos!'})
+        }
 
+        res.status(200).json({mensaje: 'Los datos ingresados son correctos', usuarioEncontrado});
     } catch (error) {
-        
+        console.error(error);
+        res.status(500).json({mensaje: 'Error al intentar loguear al usuario'});
     }
 }
