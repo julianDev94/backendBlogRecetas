729fe7b87d87a93f0323513c141d0ad70c30441b
JulianAballay <juliancabj1210@gmail.com>
2026-01-26 01:44:48 -0300

    agrego validaciones de las porciones y urlImagen

--- src/helpers/validacionesProducto.js
+++ src/helpers/validacionesProducto.js
@@ -4,8 +4,10 @@ export const validarRecetas = [
     check('titulo').notEmpty().withMessage("El titulo es obligatorio").trim().isLength({min: 4, max:50}).withMessage("El  titulo debe tener entre 4 y 50 caracteres"),
     check('categoria').notEmpty().withMessage("La categoria es obligatoria"),
     check('descripcionBreve').notEmpty().withMessage('La descripcion breve es obligatoria').trim().isLength({min: 4,max:300}).withMessage("La descripcion breve debe tener entre 4 y 300"),
-    check('tiempoPrep').notEmpty().withMessage("El tiempo de preparacion es obligatorio").trim().isInt({min: 1, max: 500}).withMessage("El tiempo de preparación debe estar entre 1 y 500"),
-    check('')
+    check('tiempoPrep').notEmpty().withMessage("El tiempo de preparacion es obligatorio").trim().isInt({min: 1, max: 500}).withMessage("El tiempo de preparación debe estar entre 1 y 500 minutos"),
+    check('tiempoCoccion').notEmpty().withMessage("El tiempo de coccion es obligatorio").trim().isInt({min: 1, max:500}).withMessage("El tiempo de cocción debe estar entre 1 y 500 minutos"),
+    check('porciones').notEmpty().withMessage("Las porciones son obligatorias").trim().isInt({min: 1, max:20}).withMessage("La cantidad de porciones debe estar entre 1 y 20"),
+    check('urlImagen').notEmpty().withMessage("La url de la imagen es obligatoria").trim().isURL(),
 
 
 ]
