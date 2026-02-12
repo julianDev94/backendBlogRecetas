39bcaa7343b5eb966d0c82dbe9115c626bc2e1d8
JulianAballay <juliancabj1210@gmail.com>
2026-02-11 22:55:52 -0300

    agrego unas validaciones para los ingredientes y pasos que consisten en validar que sean array y ademas que por cada elemento del array valide el tamaño de cada uno

--- src/helpers/validacionesRecetas.js
+++ src/helpers/validacionesRecetas.js
@@ -8,8 +8,10 @@ export const validarRecetas = [
     check('tiempoCoccion').notEmpty().withMessage("El tiempo de coccion es obligatorio").trim().isInt({min: 1, max:500}).withMessage("El tiempo de cocción debe estar entre 1 y 500 minutos"),
     check('porciones').notEmpty().withMessage("Las porciones son obligatorias").trim().isInt({min: 1, max:20}).withMessage("La cantidad de porciones debe estar entre 1 y 20"),
     check('urlImagen').notEmpty().withMessage("La url de la imagen es obligatoria").trim().isURL().withMessage("La URL de la imagen es invalida"),
-    check('ingredientes').notEmpty().withMessage("Los ingredientes son obligatorios").trim().isLength({min: 3, max:50}).withMessage("El ingrediente debe tener entre 3 y 50 caracteres"),
-    check('pasos').notEmpty().withMessage("Los pasos son obligatorios").trim().isLength({min: 3, max: 50}).withMessage("Los pasos tiene entre 3 y 50 caracteres"),
+    check('ingredientes').isArray({min: 1}).withMessage("Debe existir al menos un ingrediente"),
+    check('ingredientes.*').trim().isLength({min: 3, max:50}).withMessage("El ingrediente debe tener entre 3 y 50 caracteres"),
+    check('pasos').isArray({min: 1}).withMessage('Debe existir al menos 1 paso'),
+    check('pasos.*').trim().isLength({min: 3, max: 50}).withMessage("Los pasos tiene entre 3 y 50 caracteres"),
     (req,res,next)=>{
         
     }
